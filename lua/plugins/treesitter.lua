c46ca8bf4b3ddc8d2c6fdd8642a83bc3eb1517b4
Huijiro <huijirohankei@gmail.com>
2024-01-06 00:52:55 -0300

    Added pairing plugins

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,13 +1,20 @@
 return {
+  "windwp/nvim-ts-autotag",
+  {
     'nvim-treesitter/nvim-treesitter',
- build = ":TSUpdate",
-  config = function()
-    require('nvim-treesitter.configs').setup {
-      highlight = {
-        enable = true,
-      },
-      ensure_installed = { "lua", "vim", "typescript", "json"},
-      sync_install = false,
-    }
-  end,
+    build = ":TSUpdate",
+    config = function()
+      require('nvim-treesitter.configs').setup {
+        highlight = {
+          enable = true,
+        },
+        ensure_installed = { "lua", "vim", "typescript", "json" },
+        sync_install = false,
+        autotag = {
+          enable = true
+        }
+      }
+    end,
+  }
+
 }
