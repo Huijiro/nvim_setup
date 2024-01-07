f183b1c58a5be63fb7d0cbe0182684b125a6b995
Huijiro <huijirohankei@gmail.com>
2024-01-07 02:59:26 -0300

    Setup trouble

---
+++ lua/plugins/trouble.lua
@@ -0,0 +1,15 @@
+return {
+  "folke/trouble.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  opts = {
+
+  },
+  keys = {
+    {"<leader>xx", ":TroubleToggle<CR>", desc = "Toggle Trouble"},
+    {"<leader>xw", ":TroubleToggle workspace_diagnostics<CR>", desc = "Toggle Trouble: LSP Diagnostics (Workspace)"},
+    {"<leader>xd", ":TroubleToggle document_diagnostics<CR>", desc = "Toggle Trouble: LSP Diagnostics (Document)"},
+    {"<leader>xl", ":TroubleToggle loclist<CR>", desc = "Toggle Trouble: Location List"},
+    {"<leader>xq", ":TroubleToggle quickfix<CR>", desc = "Toggle Trouble: Quickfix"},
+
+  }
+}
