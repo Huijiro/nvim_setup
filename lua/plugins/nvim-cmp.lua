9e83a8e694857cc5a5bbd3f422a20d09362c0255
Huijiro <huijirohankei@gmail.com>
2024-04-25 23:47:13 -0300

    removed cmp settings

--- lua/plugins/nvim-cmp.lua
+++ lua/plugins/nvim-cmp.lua
@@ -28,9 +28,9 @@ return {
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
           ['<C-n>'] = cmp.mapping.select_next_item(),
           ['<C-p>'] = cmp.mapping.select_prev_item(),
-          ['<C-Space>'] = cmp.mapping.complete(),
+          ['<C-j>'] = cmp.mapping.complete(),
           ['<C-e>'] = cmp.mapping.abort(),
-          ['<C-y>'] = cmp.mapping.confirm({ select = false, }),
+          ['<C-y>'] = cmp.mapping.confirm({ select = true, }),
         }),
         view = {
           entries = {
